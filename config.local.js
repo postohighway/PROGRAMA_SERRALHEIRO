a44c29b688ade2a4fa45289d4c59caf0367688e0
postohighway <nyfwhymyfw@privaterelay.appleid.com>
2026-02-14 19:01:40 -0300

    Update config.local.js

--- config.local.js
+++ config.local.js
@@ -1,5 +1,1 @@
-ONFIG = {
-  SUPABASE_URL: "https://lnfaukysiiflparrciwz.supabase.co",
-  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuZmF1a3lzaWlmbHBhcnJjaXd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4Njk4NDEsImV4cCI6MjA4MDQ0NTg0MX0.mFBYdGIsdI00cWeou_NgBx8nNejZJeKEwK84JVKafTI",
-};
 
