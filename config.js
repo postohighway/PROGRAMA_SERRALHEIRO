47fc37862fc8677ec81a8e61253b61447a2aa144
postohighway <nyfwhymyfw@privaterelay.appleid.com>
2026-03-02 16:25:17 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js 
 (function () {
-  const SUPABASE_URL = "https://lnfaukysiiflparrciwz.supabase.co";
+  const SUPABASE_URL = 'https://lnfaukysiiflparrciwz.supabase.co';
   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuZmF1a3lzaWlmbHBhcnJjaXd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4Njk4NDEsImV4cCI6MjA4MDQ0NTg0MX0.mFBYdGIsdI00cWeou_NgBx8nNejZJeKEwK84JVKafTI',
 
   const DEFAULT_COMPANY_ID = "4e44632d-15b0-484d-bc01-ec8bff2e2189";
