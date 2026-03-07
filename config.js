c6b094021a2a2f3881638c4aaa1da60d6d27e2b9
postohighway <nyfwhymyfw@privaterelay.appleid.com>
2026-03-07 07:51:57 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js (COMMITAR no GitHub Pages)
+// Preencha com seus dados do Supabase.
+window.sbConfig = {
+  supabaseUrl: 'https://lnfaukysiiflparrciwz.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuZmF1a3lzaWlmbHBhcnJjaXd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4Njk4NDEsImV4cCI6MjA4MDQ0NTg0MX0.mFBYdGIsdI00cWeou_NgBx8nNejZJeKEwK84JVKafTI',
+
+  // Opcional, mas RECOMENDADO para o app já iniciar sem depender de buscar company_users
+  defaultCompanyId: "COLOQUE_SEU_COMPANY_ID_AQUI"
+};
