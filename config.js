7d28cc0087943b0e6c7443befd222b7231b8fc80
postohighway <nyfwhymyfw@privaterelay.appleid.com>
2026-03-07 09:01:24 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
-// config.js (COMMITAR no GitHub Pages)
-// Preencha com seus dados do Supabase.
 window.sbConfig = {
-  supabaseUrl: 'https://lnfaukysiiflparrciwz.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuZmF1a3lzaWlmbHBhcnJjaXd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4Njk4NDEsImV4cCI6MjA4MDQ0NTg0MX0.mFBYdGIsdI00cWeou_NgBx8nNejZJeKEwK84JVKafTI',
 
-  // Opcional, mas RECOMENDADO para o app já iniciar sem depender de buscar company_users
+  url: 'https://lnfaukysiiflparrciwz.supabase.co',
+
+  anon: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuZmF1a3lzaWlmbHBhcnJjaXd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4Njk4NDEsImV4cCI6MjA4MDQ0NTg0MX0.mFBYdGIsdI00cWeou_NgBx8nNejZJeKEwK84JVKafTI',
+
   defaultCompanyId: '4e44632d-15b0-484d-bc01-ec8bff2e2189'
 };
