3090cda3f4f2c6df92383a024f3d35d0f1020999
postohighway <nyfwhymyfw@privaterelay.appleid.com>
2026-03-07 08:53:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,7 @@
 // Preencha com seus dados do Supabase.
 window.sbConfig = {
   supabaseUrl: 'https://lnfaukysiiflparrciwz.supabase.co',
-  supabaseAnonKey: 'a5429d0e-347b-4476-bfe8-b0da0cfa2182
-4e44632d-15b0-484d-bc01-ec8bff2e2189',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuZmF1a3lzaWlmbHBhcnJjaXd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4Njk4NDEsImV4cCI6MjA4MDQ0NTg0MX0.mFBYdGIsdI00cWeou_NgBx8nNejZJeKEwK84JVKafTI',
 
   // Opcional, mas RECOMENDADO para o app já iniciar sem depender de buscar company_users
   defaultCompanyId: '4e44632d-15b0-484d-bc01-ec8bff2e2189'
